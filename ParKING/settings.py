d92e5ee895389eee41247a5b37610ba619fb1cef
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-03-24 19:26:03 +0100

    database update

--- ParKING/settings.py
+++ ParKING/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'ParKING.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER' : 'admin',
+        'PASSWORD': 'admin',
+        'HOST' : 'localhost',
+        'PORT' : '22',
+
     }
 }
 
