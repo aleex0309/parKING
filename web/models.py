3175e259659ca2209301101cc0b9ba6e2998f951
marcgj <mgaspjoval2002@gmail.com>
2023-04-02 21:59:11 +0200

    esquelet de les classes fet

--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Reservation(models.Model):
+    pass
+
+
+class Univesity(models.Model):
+    pass
+
+
+# La classe usuari s'ha de fer diferent per temes de la autenticacio que ja te django
+
+
+class Vehicle(models.Model):
+    pass
+
+
+class ParkingSpot(models.Model):
+    pass
+
+
+class Parking(models.Model):
+    pass
+
+
+class Reservation(models.Model):
+    pass
