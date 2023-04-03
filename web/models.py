89fabceeccf5d9509f719743e98b139655480d0f
marcgj <mgaspjoval2002@gmail.com>
2023-04-03 21:05:18 +0200

    eliminades ids redundants i canviats els str corresponents

--- web/models.py
+++ web/models.py
@@ -39,31 +39,29 @@ class VehicleUser(models.Model):
 
 
 class ParkingSpot(models.Model):
-    id_spot = models.CharField(max_length=128)
     spot_type = models.CharField(max_length=128)
     free = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'{self.id_spot}'
+        return f'{self.id}'
 
 
 class Parking(models.Model):
-    id_parking = models.CharField(max_length=128)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+
     motorbike_capacity = models.IntegerField()
     car_capacity = models.IntegerField()
     van_capacity = models.IntegerField()
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.id_parking}'
+        return f'{self.id}'
 
 
 class Reservation(models.Model):
-    id_reservation = models.CharField(max_length=128)
-    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     date = models.DateTimeField()
     price = models.FloatField()
 
     def __str__(self):
-        return f'{self.id_reservation}'
+        return f'{self.id}'
