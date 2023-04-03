69606917d4414135f621b081c57e66c06f736e3d
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-04-03 16:53:54 +0200

    Possible solution, if not change str -> repr

--- web/models.py
+++ web/models.py
@@ -11,24 +11,24 @@ class Reservation(models.Model):
     date = models.DateTimeField()
     price = models.FloatField()
 
-    def __str__(self) -> str:
-        return self.id_reservation
+    def __str__(self):
+        return f'{self.id_reservation}'
 
 
 
 class University(models.Model):
     name = models.CharField(max_length=256)
 
-    def __str__(self) -> str:
-        return self.name
+    def __str__(self):
+        return f'{self.name}'
 
 
 class UserUniversity(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     university = models.ForeignKey(University, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return str(self.user)+"@"+str(self.university)
+    def __str__(self):
+        return f'{str(self.user)+"@"+str(self.university)}'
 
 
 class Vehicle(models.Model):
@@ -38,16 +38,16 @@ class Vehicle(models.Model):
     type = models.CharField(choices=TYPES, max_length=50)
     emissions = models.CharField(max_length=1)
 
-    def __str__(self) -> str:
-        return self.plate
+    def __str__(self):
+        return f'{self.plate}'
 
 
 class VehicleUser(models.Model):
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return str(self.vehicle)+"@"+str(self.user)
+    def __str__(self):
+        return f'{str(self.vehicle)+"@"+str(self.user)}'
 
 
 class ParkingSpot(models.Model):
@@ -55,8 +55,8 @@ class ParkingSpot(models.Model):
     spot_type = models.CharField(max_length=128)
     free = models.BooleanField(default=True)
 
-    def __str__(self) -> str:
-        return self.id_spot
+    def __str__(self):
+        return f'{self.id_spot}'
 
 
 class Parking(models.Model):
@@ -66,5 +66,5 @@ class Parking(models.Model):
     van_capacity = models.IntegerField()
     university = models.ForeignKey(University, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return self.id_parking
+    def __str__(self):
+        return f'{self.id_parking}'
