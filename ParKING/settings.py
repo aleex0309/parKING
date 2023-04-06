b63626965b01f184c804cb25ab711998bcf3f6fb
peremunoz <peremunozfiguerol33@gmail.com>
2023-04-06 18:56:41 +0200

    Change redirection when login to main page

--- ParKING/settings.py
+++ ParKING/settings.py
@@ -119,6 +119,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
