c771402005c6e6c19c0fd41d0cfd8efdfca6be14
peremunoz <peremunozfiguerol33@gmail.com>
2023-04-06 18:49:46 +0200

    Add url mappings for account managing

--- ParKING/urls.py
+++ ParKING/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wv.home, name='home'), 
     path('university/<int:id_university>/', wv.university, name='parkings'),
-    path('university/<int:id_university>/parking/<int:id_parking>', wv.parking, name='parkingSpots')
+    path('university/<int:id_university>/parking/<int:id_parking>', wv.parking, name='parkingSpots'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
