494ef7cb1d3ecf12f751a756d79eee4c8e21551f
marcgj <mgaspjoval2002@gmail.com>
2023-05-04 15:21:52 +0200

    reservations history done

--- web/views.py
+++ web/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from web.models import University, Parking, ParkingSpot, TYPES
+from django.http import HttpResponse, HttpRequest, Http404
+from web.models import University, Parking, ParkingSpot, TYPES, Reservation
 
 # Create your views here.
 
+
 def home(request):
     universities = University.objects.all()
-    return render(request, "web/index.html", {"universities":universities})
+    return render(request, "web/index.html", {"universities": universities})
+
 
 def university(request, id_university):
     try:
@@ -14,14 +16,26 @@ def university(request, id_university):
     except University.DoesNotExist:
         raise Http404("University does not exist")
     parkings = Parking.objects.filter(university=university)
-    return render(request, "web/university.html", {"university":university, "parkings":parkings})
+    return render(request, "web/university.html", {"university": university, "parkings": parkings})
+
 
 def parking(request, id_university, id_parking):
-    try: 
+    try:
         parking = Parking.objects.get(id=id_parking, university=id_university)
     except Parking.DoesNotExist:
         raise Http404("Parking does not exist")
     spots = ParkingSpot.objects.filter(parking=id_parking)
-    car_spots = ParkingSpot.objects.filter(parking=id_parking, type=TYPES[1][1])
-    motorbike_spots = ParkingSpot.objects.filter(parking=id_parking, type=TYPES[0][1])
-    return render(request, "web/parking.html", {"parking":parking, "spots":spots, "count_cars":len(car_spots), "count_motorbikes":len(motorbike_spots)})
+    car_spots = ParkingSpot.objects.filter(
+        parking=id_parking, type=TYPES[1][1])
+    motorbike_spots = ParkingSpot.objects.filter(
+        parking=id_parking, type=TYPES[0][1])
+    return render(request, "web/parking.html", {"parking": parking, "spots": spots, "count_cars": len(car_spots), "count_motorbikes": len(motorbike_spots)})
+
+
+def dashboard(request: HttpRequest):
+    user = request.user
+
+    reservations = Reservation.objects.filter(user=user)
+
+    return render(request, "web/dashboard.html",
+                  {"user": user, "reservations": reservations})
