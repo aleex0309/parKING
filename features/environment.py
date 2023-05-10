60cb5b4813fbac9eeee9c3612b03b82e7096c839
marcgj <mgaspjoval2002@gmail.com>
2023-05-10 19:16:29 +0200

    added no sandbox so can work with root user

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,11 @@
 from splinter.browser import Browser
 
+chrome_options = ['--no-sandbox']
+
+
 def before_all(context):
-    context.browser = Browser('chrome', headless=True)
+    context.browser = Browser('chrome', headless=True, options=chrome_options)
+
 
 def after_all(context):
     context.browser.quit()
