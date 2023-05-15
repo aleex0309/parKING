4c911202fa299e101e7dd949dae310a431772241
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-05-15 22:10:01 +0200

    added static routes

--- ParKING/settings.py
+++ ParKING/settings.py
@@ -125,7 +125,9 @@ LOGIN_REDIRECT_URL = 'dashboard'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
